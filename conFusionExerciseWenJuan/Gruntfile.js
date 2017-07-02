339cf7c0ee4bbc8745fafe539b04775452172d66
cxspace <442961832@qq.com>
2017-07-02 23:06:34 +0800

    整个

--- conFusionExerciseWenJuan/Gruntfile.js
+++ conFusionExerciseWenJuan/Gruntfile.js
@@ -181,7 +181,7 @@ connect: {
     port: 9000,
     // Change this to '0.0.0.0' to access the server from outside.
     hostname: 'localhost',
-    livereload: 35729
+    livereload: 35730
   },
 
   dist: {
